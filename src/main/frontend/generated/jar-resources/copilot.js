e95d19b19ded95f8f3477b781807e9025b86ae1d
Valdemar Sinkievic <v.sinkievic@bpm-id.com>
2025-11-10 18:00:08 +0200

    Gitignore updated according to Vaadin recommendations

--- src/main/frontend/generated/jar-resources/copilot.js
+++
@@ -1,5 +0,0 @@
-import { at as t, as as a } from "./copilot/copilot-CmrCuzn5.js";
-export {
-  t as _createChildrenDefinitions,
-  a as _registerImporter
-};
--- src/main/frontend/generated/theme.js
+++
@@ -1,2 +0,0 @@
-import {applyTheme as _applyTheme} from './theme-vapp-theme.generated.js';
-export const applyTheme = _applyTheme;
